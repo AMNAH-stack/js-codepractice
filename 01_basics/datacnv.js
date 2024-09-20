f4a5acc9f0bf47e817e81bb9efa2d69e1a94d244
AMNAH-stack <amnanoor4733@gmail.com>
2024-09-20 17:07:42 +0000

    conversion

---
+++ 01_basics/datacnv.js
@@ -0,0 +1,20 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof (score))
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+// "33" => 33 
+// "33abc"=> NaN
+// true=>0,false=>1
+let isLoggedIn= "amna"
+
+let booleanIsLogged = Boolean(isLoggedIn)
+console.log(booleanIsLogged);
+//1 => true
+//"" => false
+//"amna" => true
+let someNumber = 22
+let valueInString = String(someNumber)
+console.log(valueInString)
+console.log(typeof valueInString)
+
---
+++ 01_basics/var.js
@@ -0,0 +1,5 @@
+const accountId = 1234
+let accountName = "amna@gmail.com"
+var accountPass = "55555"
+accontcity = "kunjah"
+console.table([accountId,accountName,accountPass,accontcity])
