4e839e4b4288f88860798f80dcdb201d7f1c67a4
AMNAH-stack <amnanoor4733@gmail.com>
2024-09-20 19:24:04 +0000

    datatypes

--- 01_basics/datacnv.js
+++ 01_basics/datacnv.js
@@ -1,20 +1,48 @@
 let score = "33"
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+//console.log(typeof (score))
 let valueInNumber= Number(score)
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 // "33" => 33 
 // "33abc"=> NaN
 // true=>0,false=>1
 let isLoggedIn= "amna"
 
 let booleanIsLogged = Boolean(isLoggedIn)
-console.log(booleanIsLogged);
+//console.log(booleanIsLogged);
 //1 => true
 //"" => false
 //"amna" => true
 let someNumber = 22
 let valueInString = String(someNumber)
-console.log(valueInString)
-console.log(typeof valueInString)
+//console.log(valueInString)
+//console.log(typeof valueInString)
+// OPERATIONS //
+let value = 3
+let negValue  = -value
+//console.log(negValue)
+{
+let a =3
+let b =++a
+//console.log(a,b) //a=a+1
+let x=2
+let y=x++
+//console.log(x,y) 
+}
+{
+let a=6
+let b = --a
+//console.log(a , b)
+let x=8
+let y = x--
+//console.log(x,y)
+}
+//console.log("2" > 1)
+//console.log(null > 0) //js main comparisons operators differently behave krty haain or wo null ko number
+//console.log(null == 0)// main convert kr dety hai lakin assignment ooperartor nhi assignment k liya null 
+//console.log(null >= 0)//NOT A NUMBER (NaN) k tor pr act krta ha
+ 
 
+//console.log(undefined > 0)
+//console.log(undefined == 0)
+//console.log(undefined >= 0)
---
+++ 01_basics/datatype.js
@@ -0,0 +1,20 @@
+const score = 100;
+let name = "Amna Noor";
+let loggedIn = true;
+let username;
+BigInt = 435356789956354673254n;
+let outSitTemp =null;
+let iD = Symbol('123')
+let anotherId = Symbol('123')
+//console.log(iD === anotherId)
+
+const students =["AMNA" , "SAJAL" ,"ZAINAB","ZEBA"]
+let myinfo={
+    name:"AMNA",
+    age:21, 
+
+}
+const myfunction = function(){
+    console.log("HELLO WORLD")
+}
+console.log(typeof outSitTemp)
