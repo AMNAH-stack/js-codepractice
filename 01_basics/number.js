a88a5fcbf2fe04ba12ce6251ff5474ddd354e74c
AMNAH-stack <amnanoor4733@gmail.com>
2024-09-23 17:01:13 +0000

    numbers

---
+++ 01_basics/number.js
@@ -0,0 +1,22 @@
+const score = 100
+console.log(score)
+const balance = new Number(400)
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(typeof balance)
+console.log(balance.toFixed(3))//point k bad itni values k khyal rkhna ha
+
+const newNum = 236.4444
+console.log(newNum.toPrecision(4))//ye btata ha k kitni value pr focus krna ha precision ki
+ 
+const hundreds =10000000
+console.log(hundreds.toLocaleString('en-IN'));
+//MATHS 
+console.log(Math)
+console.log(Math.abs(-5))
+console.log(Math.round(5.56))
+console.log(Math.ceil(6.7))
+console.log(Math.floor(8.1))
+console.log(Math.min(4,5,6,8,9,2,10))
+console.log(Math.max(4,5,6,8,9,2,10))
